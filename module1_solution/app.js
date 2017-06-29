1486d3a4c670931d4d455f94e81e286db6136d74
Michael <cwithmichael@gmail.com>
2017-06-29 16:52:01 -0500

    added implementation for bonus requirements

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -13,14 +13,19 @@
 		$scope.checkLunch = function () {
 			if (!$scope.lunch){
 				$scope.message = 'Please enter data first';
-				return;
+				return 'red';
 			}
 			var lunchItems = $scope.lunch.split(',');
 			var parsedItems = lunchItems.filter(function (w) {return w;});
-			if (parsedItems.length <= 3)
+			if (parsedItems.length <= 3) {
 				$scope.message = 'Enjoy!';
-			else
+				return {"color":"green", "border":"solid 2px green"};
+			}
+			else {
 				$scope.message = 'Too Much!';
+				return {"color":"red", "border":"solid 2px red"};
+			}
 		};
+
 	}
 })();
